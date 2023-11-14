450e788fcb7736eedc848c2218f34507f1c3fada
gk <iba3374@naver.com>
2023-11-14 10:20:17 +0900

    init commit

---
+++ src/main/java/com/sparta/nbcamptodo/NbcampTodoApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.nbcamptodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcampTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbcampTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/nbcamptodo/NbcampTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.nbcamptodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbcampTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
