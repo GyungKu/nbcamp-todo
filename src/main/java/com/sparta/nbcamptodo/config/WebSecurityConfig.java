004d7e994c003212dc1d5e970976d8699bd5b208
gk <iba3374@naver.com>
2023-11-15 19:37:42 +0900

    시큐리티 필터 할 일 조회 인증 없이 허가

--- src/main/java/com/sparta/nbcamptodo/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/nbcamptodo/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -61,9 +62,9 @@ public class WebSecurityConfig {
         http.authorizeHttpRequests((authorizeHttpRequests) ->
                 authorizeHttpRequests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
-                        .requestMatchers("/").permitAll() // 메인 페이지 요청 허가
-                        .requestMatchers("/api/user/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
-                        .anyRequest().authenticated() // 그 외 모든 요청 인증처리
+                        .requestMatchers(HttpMethod.POST, "/api/todo/**").authenticated() // /api/todo/로 시작하는 post 요청 인증처리
+                        .requestMatchers(HttpMethod.PATCH, "/api/todo/**").authenticated() // patch요청 인증처리 (수정)
+                        .anyRequest().permitAll() // 그 외 모든 요청 허가 (회원가입, 로그인, 투두 목록&상세 조회
         );
         // 필터 관리
         http.addFilterBefore(jwtAuthorizationFilter(), JwtAuthenticationFilter.class);
