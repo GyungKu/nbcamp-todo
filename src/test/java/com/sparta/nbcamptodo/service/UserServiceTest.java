08829a0fc8ac527434d9c0e6fdf3d862fe327177
gk <iba3374@naver.com>
2023-12-01 16:15:17 +0900

    test(User) : 유저서비스 회원가입 실패 테스트(중복 회원 이름)

--- src/test/java/com/sparta/nbcamptodo/service/UserServiceTest.java
+++ src/test/java/com/sparta/nbcamptodo/service/UserServiceTest.java
@@ -1,12 +1,17 @@
 package com.sparta.nbcamptodo.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.sparta.nbcamptodo.dto.SignRequestDto;
+import com.sparta.nbcamptodo.entity.User;
+import com.sparta.nbcamptodo.exception.DuplicateUsernameException;
 import com.sparta.nbcamptodo.repository.UserRepository;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -32,6 +37,21 @@ class UserServiceTest {
 
         // then
         assertThat(message).isEqualTo("회원 가입 성공");
+    }
+
+    @Test
+    @DisplayName("회원가입 중복 회원 테스트")
+    void test2() {
+        //given
+        SignRequestDto requestDto = new SignRequestDto("userA", "12345678");
+        UserService userService = new UserService(userRepository, passwordEncoder);
+        User user = new User(requestDto);
+        BDDMockito.given(userRepository.findByUsername(requestDto.getUsername()))
+            .willReturn(Optional.of(user));
+
+        //when - then
+        assertThatThrownBy(() -> userService.signup(requestDto))
+            .isInstanceOf(DuplicateUsernameException.class);
     }
 
 }
