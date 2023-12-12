e33db8c97eed9c8ae1c423db36d6a774be6e5a96
gk <iba3374@naver.com>
2023-12-12 12:55:05 +0900

    feat: Controller 걸린 시간 재는 AOP 추가

---
+++ src/main/java/com/sparta/nbcamptodo/aop/TimeAop.java
@@ -0,0 +1,20 @@
+package com.sparta.nbcamptodo.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j(topic = "걸린 시간 기록")
+public class TimeAop {
+
+    @Around("execution(* com.sparta.nbcamptodo.controller..*(..))")
+    public void timeLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        joinPoint.proceed();
+        log.info("method = {}, 걸린 시간 = {}ms", joinPoint.getSignature().getName(), System.currentTimeMillis() - start);
+    }
+}
