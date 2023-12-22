212da21d8dd61968ebd4f35b06685f3bb44bea70
gk <iba3374@naver.com>
2023-12-22 17:02:56 +0900

    test(Todo): 할 일 검색 조회 Repository 테스트 추가

--- src/test/java/com/sparta/nbcamptodo/repository/TodoRepositoryTest.java
+++ src/test/java/com/sparta/nbcamptodo/repository/TodoRepositoryTest.java
@@ -1,14 +1,15 @@
 package com.sparta.nbcamptodo.repository;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import com.sparta.nbcamptodo.config.JpaConfig;
 import com.sparta.nbcamptodo.config.QuerydslConfig;
 import com.sparta.nbcamptodo.dto.SignRequestDto;
+import com.sparta.nbcamptodo.dto.SortDto;
+import com.sparta.nbcamptodo.dto.TodoCondition;
 import com.sparta.nbcamptodo.dto.TodoRequestDto;
 import com.sparta.nbcamptodo.entity.Todo;
 import com.sparta.nbcamptodo.entity.User;
-import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,6 +18,9 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
@@ -55,6 +59,41 @@ public class TodoRepositoryTest {
         assertThat(todoList.get(0).getTitle()).isEqualTo(todo3.getTitle());
         assertThat(todoList.get(1).getTitle()).isEqualTo(todo2.getTitle());
         assertThat(todoList.get(2).getTitle()).isEqualTo(todo1.getTitle());
+    }
+
+    @Test
+    @DisplayName("검색 테스트")
+    void searchTest() {
+        //given
+        User user = new User(new SignRequestDto("user1", "12345678"));
+        userRepository.save(user);
+        Todo todo1 = new Todo(new TodoRequestDto(
+            "제목1", "내용1"), user);
+        Todo todo2 = new Todo(new TodoRequestDto(
+            "제목2", "내용2"), user);
+        Todo todo3 = new Todo(new TodoRequestDto(
+            "제목3", "내용3"), user);
+        todoRepository.save(todo1);
+        todoRepository.save(todo2);
+        todoRepository.save(todo3);
+
+        /**
+         * and가 아닌 or이므로, 제목, 내용 둘 중 하나라도 일치하면 꺼내온다.
+         * 아래서 꺼내오는 데이터는 2개, 제목이 제목1인 것과 내용이 내용2인 것
+         * 생성일 기준으로 내림차순 정렬 하기에 내용2가 먼저, 제목1이 다음이다.
+         */
+        TodoCondition cond = new TodoCondition("제목1", "내용2");
+        Pageable pageable = PageRequest.of(0, 2);
+        SortDto sortDto = new SortDto("createdAt", false);
+
+        //when
+        Page<Todo> todos = todoRepository.searchTodoList(cond, pageable, sortDto);
+        List<Todo> todoList = todos.getContent();
+
+        //then
+        assertThat(todos.getTotalElements()).isEqualTo(2);
+        assertThat(todoList.get(0).getTitle()).isEqualTo(todo2.getTitle());
+        assertThat(todoList.get(1).getContent()).isEqualTo(todo1.getContent());
     }
     
 }
