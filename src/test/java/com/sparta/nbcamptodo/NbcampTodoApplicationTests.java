f49d443389b117165d3ac576047e53c9d87bda14
gk <iba3374@naver.com>
2023-12-22 14:21:38 +0900

    fix: 기존 properties 파일에 include 제거

--- src/test/java/com/sparta/nbcamptodo/NbcampTodoApplicationTests.java
+++ src/test/java/com/sparta/nbcamptodo/NbcampTodoApplicationTests.java
@@ -1,9 +1,14 @@
 package com.sparta.nbcamptodo;
 
+import com.sparta.nbcamptodo.config.JpaConfig;
+import com.sparta.nbcamptodo.config.QuerydslConfig;
+import com.sparta.nbcamptodo.config.S3Config;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import({JpaConfig.class, S3Config.class, QuerydslConfig.class})
 class NbcampTodoApplicationTests {
 
 	@Test
